0f34ca69d9a59edf1f45daed436c1e7abae7ae8d
khojwar <khojwartikaram@gmail.com>
2025-05-25 16:07:19 +0545

    modifying src/lib/axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "https://leetlab-backend.onrender.com/api/v1",
     withCredentials: true,
 })
